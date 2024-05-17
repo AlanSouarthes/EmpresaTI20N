777985a3314db3a12b723f178d627556df285b50
ALAN OLIVEIRA SOUARTHES <alan.osouarthes@senacsp.edu.br>
2024-05-16 22:10:00 -0300

    Primeiro Commit

---
+++ PessoaTI20N/DAO.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;//importar o MySQL.
+
+namespace PessoaTI20N
+{
+    class DAO
+    {
+        public MySqlConnection conexao;
+        public long[] cpf;
+        public string[] nome;
+        public string[] telefone;
+        public string[] endereco;
+        public int i;
+        public int contador;
+
+        public DAO() 
+        {
+            conexao = new MySqlConnection("servcer=localhost;Database=empresaTI20N;Uid=root;password=");
+            try
+            {
+                conexao.Open();//Abrir a conexão
+                MessageBox.Show("Conectado!");
+            }
+            catch(Exception ex)
+
+            {
+                MessageBox.Show("Algo deu errado !!\n\n" + ex);
+            }
+        }//Fim do construtor
+
+        public string Inserir(long cpf, string nome, string telefone, string endereco)
+        {
+            string inserir = $"Insert into pessoa(cpf, nome, telefone, endereco) values" +
+                $"('{cpf}','{nome}','{telefone}','{endereco}')";
+
+            MySqlCommand sql = new MySqlCommand(inserir, conexao);
+            string resultado = sql.ExecuteNonQuery() + " Executado!";
+            return resultado;
+        }//Fim do método
+
+        public void PreencherVetor()
+        {
+            string query = "select * from pessoa";
+
+            //Instanciar
+            this.cpf = new long[100];
+            this.nome = new string[100];
+            this.telefone = new string[100];
+            this.endereco = new string[100];
+
+            // Fazer o comando deSeleçao do banco
+            MySqlCommand sql = new MySqlCommand(query, conexao);
+            //Leitor do Banco
+            MySqlDataReader leitura = sql.ExecuteReader();
+
+            i = 0;
+            contador = 0;
+            while(leitura.Read())
+            {
+                cpf[i]       = Convert.ToInt64(leitura["cpf"]);
+                nome[i]      = leitura["nome"] + "";
+                telefone[i]  = leitura["telefone"] + "";
+                endereco[i] = leitura["endereco"] + "";
+                i++;//Preencher o vetor
+                contador++;//Contar quantos dados eu tenho
+            }//Fim do While
+
+            //Encerro a comunicação com o software
+            leitura.Close();
+        }//Fim do preencher
+
+        //criar o metodo para retornar o contaror
+
+        public int QuantidadedeDados()
+        {
+            return contador;
+        }//Fim do quantidade de dados
+
+        public string Atualizar(long CPF, string nomeTabela, string campo, string dado )
+        {
+            string query =$"updade {nomeTabela} set {campo} = '{dado}' where cpf = '{cpf}'";
+            MySqlCommand sql = new MySqlCommand(query, conexao);
+            string resultado = sql.ExecuteNonQuery() + "Atualizado!";
+            return resultado;
+        }//Fim do metodo
+
+        public string Excluir(long cpf, string nomeTabela)
+        {
+            string query = $"delete from {nomeTabela}; where CPF = '{cpf}'";
+            MySqlCommand sql = new MySqlCommand(query, conexao);
+            string resultado = sql.ExecuteNonQuery() + "Excluído!";
+            return resultado;
+        }//Fim do Excluir
+    }//Fim da Classe
+}//Fim do Projeto
---
+++ PessoaTI20N/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace PessoaTI20N
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Escolha = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Escolha
+            // 
+            this.Escolha.AutoSize = true;
+            this.Escolha.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Escolha.Location = new System.Drawing.Point(61, 40);
+            this.Escolha.Name = "Escolha";
+            this.Escolha.Size = new System.Drawing.Size(230, 18);
+            this.Escolha.TabIndex = 0;
+            this.Escolha.Text = "Escolha uma das opçoes abaixo:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cadastrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 62);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Consultar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(112, 272);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 62);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Atualizar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(112, 357);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 62);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Excluir";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Escolha);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Escolha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
